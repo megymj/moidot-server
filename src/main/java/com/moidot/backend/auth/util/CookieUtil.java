8e543b78a3761a6c3af58c580b9b43ba92171e64
Moojun <mgim32137@gmail.com>
2025-07-23 22:13:11 +0900

    쿠키 HTTP도 전송 허용

--- src/main/java/com/moidot/backend/auth/util/CookieUtil.java
+++ src/main/java/com/moidot/backend/auth/util/CookieUtil.java
@@ -10,7 +10,7 @@ public class CookieUtil {
     public void addRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
         Cookie cookie = new Cookie("refreshToken", refreshToken);
         cookie.setHttpOnly(true);  // JS 접근 차단
-        cookie.setSecure(true);    // HTTPS 전송만 허용
+//        cookie.setSecure(true);    // HTTPS 전송만 허용
         cookie.setPath("/");       // 모든 요청에 포함
         cookie.setMaxAge(60 * 60 * 24 * 14); // 14일
         response.addCookie(cookie);
