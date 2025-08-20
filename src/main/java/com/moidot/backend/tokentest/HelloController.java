8ba999e323be55720b615a379fd9ca0575c806a4
Moojun <mgim32137@gmail.com>
2025-08-20 23:37:40 +0900

    어노테이션 수정

--- src/main/java/com/moidot/backend/tokentest/HelloController.java
+++ src/main/java/com/moidot/backend/tokentest/HelloController.java
@@ -1,11 +1,11 @@
 package com.moidot.backend.tokentest;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/token-test")
-@Controller
+@RestController
 public class HelloController {
 
     @GetMapping
