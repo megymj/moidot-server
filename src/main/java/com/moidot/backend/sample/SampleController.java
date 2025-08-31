24d52f31f712dd4d81652c78336f1a7dbe6bdf1c
Moojun <mgim32137@gmail.com>
2025-08-31 16:39:23 +0900

    테스트용 샘플 컨트롤러 수정

--- src/main/java/com/moidot/backend/sample/SampleController.java
+++ src/main/java/com/moidot/backend/sample/SampleController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/token-test")
+@RequestMapping("/api/sample")
 @RestController
-public class HelloController {
+public class SampleController {
 
     @GetMapping
     public Object hello() {
-        return null;
+        return "Sample Test API Success!";
     }
 }
