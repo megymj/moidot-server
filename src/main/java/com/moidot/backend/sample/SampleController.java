1a4646fd17924e10d3e09312bf318869e2437361
Moojun <mgim32137@gmail.com>
2025-09-07 00:36:23 +0900

    로깅 설정 파일 추가(local/prd)

--- src/main/java/com/moidot/backend/sample/SampleController.java
+++ src/main/java/com/moidot/backend/sample/SampleController.java
@@ -1,15 +1,18 @@
 package com.moidot.backend.sample;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/api/sample")
 @RestController
 public class SampleController {
 
     @GetMapping
     public Object hello() {
+        log.info("Sample Test API Called");
         return "Sample Test API Success!";
     }
 }
